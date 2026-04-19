2d81a32355a23138c298f2696ab00c217ec9a6c0
Cosmic <173505583+usernamenotfound11@users.noreply.github.com>
2026-04-19 22:44:23 +0300

    Remade a bit
    added parsing and started rewriting logic of creating&sending payloads(maybe create my own protocol)

--- client.c
+++ client.c
@@ -7,6 +7,90 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <openssl/evp.h>
+#include <openssl/aes.h>
+
+void rem_wrong(char* input) {
+        char* g = input;
+        while(*input) {
+        if(*input >= 'a' && *input <= 'z') {
+        *g = *input;
+        g++;
+                }
+        input++;
+        }
+        *g = '\0';
+}
+
+void clear_arg(char* input) {
+        char* g = input;
+        while(*input) {
+        if((*input >= 'a' && *input <= 'z') || (*input == '.') || (*input >= '0' && *input <='9')) {
+        *g = *input;
+        g++;
+                }
+        input++;
+        }
+        *g = '\0';
+}
+
+void parse_copy_args(char input[50] , int com_len , char args[2][15]) {
+        int j = 0;
+        while(input[com_len]!='-') {
+        args[0][j] = input[com_len];
+        com_len++;
+        j++;
+        if(j > 14)break;
+        }
+
+        args[0][j] = '\0';
+        clear_arg(args[0]);
+
+        int arg1_len = strlen(args[0]);
+        memcpy(args[1] , input + com_len , 14);
+        args[1][14] = '\0';
+        clear_arg(args[1]);
+
+}
+
+void prepare_payload(char payload[100] , char command[5] , char args[2][15]);
+
+int function_check(char* buffer) {
+
+        if(buffer == NULL) {
+        return 0;
+        }
+
+        if(strcmp(buffer,"echo") == 0) {
+        return 22;
+        }
+
+        if(strcmp(buffer, "exit") == 0) {
+        return 43;
+        }
+
+        if(strcmp(buffer, "creat") == 0) {
+        return 23;
+        }
+
+        if(strcmp(buffer, "cp") == 0) {
+        return 24;
+        }
+
+        if(strcmp(buffer , "rm") == 0) {
+        return 25;
+        }
+
+        if(strcmp(buffer , "ls") == 0) {
+        return 30;
+        }
+
+	if(strcmp(buffer , "put") == 0) {
+	return 32;
+	}
+
+        return 0;
+}
 
 
 int main() {
@@ -57,25 +141,68 @@ int main() {
 
 	char auth[5];
 	int ready = 0;
-	recv(server_socket , auth , 4 , 0);
+	ssize_t rr3 = recv(server_socket , auth , 4 , 0);
+	if(rr3 <= 0) close(server_socket);
+	if(rr3 > 0 && rr3 < 5) auth[rr3] = '\0';
 	if(strstr(auth , "ACT\n") != 0) {
 	ready = 1;
 	}
+	if(strstr(auth , "EXI\n") != 0) {
+	close(server_socket);
+	}
+
+
+
+
 
 	if(ready == 1) {
-	char *input = calloc(50 , sizeof(char));
-	char *got = calloc(500 , sizeof(char));
+	char input[50];
+	char payload[100];
+	char got[500];
+	int function = 0;
+	int dash_count = 0;
+	char command[5];
+	char args[2][15];
+
 	ssize_t r = read(0 , input , 49);
 
 	if(r > 0) input[r] = '\0';
 	if(r <= 0) close(server_socket);
 
+	memcpy(command , input , 4);
+	command[4] = '\0';
+	rem_wrong(command);
+
+	function = function_check(command);
+
+	if(function == 24) {
+
+	for(int i = 0; i < r ; i++) {
+	if(input[i] == '-')dash_count++;
+	}
+
+	if(dash_count <= 0 || dash_count > 1) {
+	send(server_socket , "DED\n" , 4 , 0);
+	close(server_socket);
+	}
+
+	if(dash_count == 1) {
+	int com_len = strlen(command);
+	parse_copy_args(input, com_len , args);
+	}
+
+	}
+
+}
+
+
+/*
 	const char *buffer = input;
 
 	send(server_socket , buffer , r , 0);
 
 	int len = 499;
-	if(strstr(buffer , "ls") != 0) {
+	if(function == 30) {
 	ssize_t d = 0;
 	while(( d = recv(server_socket , got , 499 , 0)) > 0) {
 
@@ -90,6 +217,7 @@ int main() {
 }
 
 }
+*/
 	close(server_socket);
 
 }
